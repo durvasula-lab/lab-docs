38ded9d8b72b9587c638fcafbc6a15c5233a97ab
C. Cataldo-Ramirez <43756152+ccatram@users.noreply.github.com>
2026-01-20 11:13:37 -0800

    Update merge_outs.sh
    fixed column matching syntax

--- software_specific/S-LDXR/merge_outs.sh
+++ software_specific/S-LDXR/merge_outs.sh
@@ -3,7 +3,7 @@
 # Define input and output files
 BIN_FILE="all_bin_annots.out"
 CONT_FILE="all_binned_contannots.out"
-OUTPUT_FILE="sldxr_out_[OUTPUT NAME HERE].txt"
+OUTPUT_FILE="<OUTPUT_FILE_NAME>.txt"
 
 # Define continuous valued annotations to exclude
 CONT_ANNOTS="GERP.NS|MAF_Adj_Predicted_Allele_Age|MAF_Adj_LLD_AFR|Recomb_Rate_10kb|Nucleotide_Diversity_10kb|Backgrd_Selection_Stat|CpG_Content_50kb|MAF_Adj_ASMC"
@@ -19,8 +19,12 @@ head -n 1 "$BIN_FILE" > temp_header.txt
 awk -F'\t' 'NR==1 {for(i=1;i<=NF;i++) print i,$i}' "$BIN_FILE" > temp_cols.txt
 
 # Get column numbers to keep (exclude TAU1, TAU1_SE, TAU2, TAU2_SE, THETA, THETA_SE, GCOR, GCOR_SE)
-COLS_TO_KEEP=$(awk -F' ' -v remove="$REMOVE_COLS" '
-    $2 !~ remove {printf "%s%s", (NR==1?"":","), $1}
+# Use exact match (with tabs) to avoid matching GCORSQ when looking for GCOR
+COLS_TO_KEEP=$(awk -F' ' '
+    $2 != "TAU1" && $2 != "TAU1_SE" && $2 != "TAU2" && $2 != "TAU2_SE" && 
+    $2 != "THETA" && $2 != "THETA_SE" && $2 != "GCOR" && $2 != "GCOR_SE" {
+        printf "%s%s", (NR==1?"":","), $1
+    }
 ' temp_cols.txt)
 
 # Process binary annotations file: exclude continuous annots and remove specific columns
@@ -48,8 +52,8 @@ $3 !~ cont {
     print ""
 }' "$BIN_FILE" > temp_binned.txt
 
-# Combine binned and continuous files (continuous file already has correct columns)
-cat temp_binned.txt "$CONT_FILE" | awk 'NR==1 || !/^P1/' > "$OUTPUT_FILE"
+# Combine binned and continuous files (skip header from continuous file)
+(cat temp_binned.txt && tail -n +2 "$CONT_FILE") > "$OUTPUT_FILE"
 
 # Clean up temporary files
 rm -f temp_header.txt temp_cols.txt temp_binned.txt
